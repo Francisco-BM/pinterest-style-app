6680c9523202db9a788506a0ab5eebf9ebcaea19
Francisco <fbustillos2014@gmail.com>
2021-09-03 20:01:07 +0200

    subida a github uno

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/Pint',{
+ useUnifiedTopology:true,
+
+
+ })
+
+ .then(db=>console.log(`DB is conected`))
+ .catch(err=>console.error(err));
---
+++ src/models/Image.js
@@ -0,0 +1,20 @@
+const{Schema,model}=require('mongoose');
+
+const imageSchema=new Schema({
+
+title:{type:String},
+description:{type:String},
+filename:{type:String},
+path:{type:String},
+originalname:{type:String},
+mimetype:{type:String},
+size:{type:Number},
+created_at:{type:Date,default:Date.now()}//esto es para colocar la fecha 
+
+})
+
+ module.exports=model('Image',imageSchema);
+
+
+
+ 
